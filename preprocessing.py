fcec1f8cb2b5f379e2a6aa90d0d2f9b46e322ffe
Jean-Charles <jeancharles.cc@gmail.com>
2021-03-21 02:39:05 +0000

    Renamed python files for clarity

--- preprocessing.py
+++ preprocessing.py
@@ -1,24 +1,31 @@
 import h5py
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-from skimage  import transform
+from skimage  import transform, util
+import scipy
+import numpy as np
 
-f = h5py.File('train_img/train_0.h5py','r')
 
-
-#print keys
-keys = list(f.keys())
-img_0 = f[keys[0]]
-
-img = img_0['11800'][:]
-plt.imshow(img[:])
-plt.show()
-
-img_trans = transform.rescale(img,0.5,anti_aliasing=True)
-plt.imshow(img_trans)
-plt.show()
 
 def read_img(img):    
     img = mpimg.imread(img)
-    img = transform.rescale(img,0.5,anti_aliasing=True)
+    img = util.invert(img)
+    img = scipy.sparse.csr_matrix(img)
     return img
+
+#f = h5py.File('train_img/train_0.h5py','r')
+
+
+#print keys
+#keys = list(f.keys())
+#img_0 = f[keys[0]]
+
+#img = img_0['1100'][:]
+
+
+#test = scipy.sparse.csr_matrix(util.invert(img))
+
+#plt.imshow(util.invert(img))
+#plt.show()
+
+
